1ca70982ab243dbe95fb3941cbef827c4fe68b0f
YusufCoding <haryadim28@gmail.com>
2024-08-06 18:11:29 +0700

    Update TestCase.php for PHPUnit testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,7 +3,10 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\User;
 use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class ExampleTest extends TestCase
 {
@@ -12,7 +15,11 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $user = User::where('email', 'yusuf@mail.com')->first();
+
+        $token = JWTAuth::fromUser($user);
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->get('/user');
 
         $response->assertStatus(200);
     }
