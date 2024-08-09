129180bcd2e2ae3e1351947c240f01ceb0443488
YusufCoding <haryadim28@gmail.com>
2024-08-09 14:59:02 +0700

    Update develop branch with feature/auth & add route for check info app

---
+++ app/Http/Controllers/API/InfoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InfoController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'status' => 200,
+            'message' => 'Info Simple Application'
+        ], 200);
+    }
+}
