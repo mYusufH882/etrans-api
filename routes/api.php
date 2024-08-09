ec8654864dca2606df93b454acad416900e13595
YusufCoding <haryadim28@gmail.com>
2024-08-09 14:39:38 +0700

    Recommit changes before

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/login', AuthController::class);
-Route::get('/info', [InfoAppController::class, 'index']);
 
 Route::middleware('auth:api')->group(function() {
     Route::get('/user', [AuthController::class, 'userProfile']);
