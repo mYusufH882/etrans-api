0c6cad87c3e7419c3cf3dc1c35cf94e65160bedc
YusufCoding <haryadim28@gmail.com>
2024-08-14 11:09:07 +0700

    Update info version

--- app/Http/Controllers/API/InfoController.php
+++ app/Http/Controllers/API/InfoController.php
@@ -11,7 +11,7 @@ class InfoController extends Controller
     {
         return response()->json([
             'status' => 200,
-            'message' => 'Info Simple Application v1.x.x'
+            'message' => 'Info Simple Application v1.1.0'
         ], 200);
     }
 }
