1842c3deb62ee3da7aabe4d00e1aebcc04ef6334
YusufCoding <haryadim28@gmail.com>
2024-08-29 11:33:55 +0700

    Update graphic data for statistic sales per month and show growthRate data

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Traits\ApiResponse;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -21,13 +22,35 @@ class DashboardController extends Controller
             )
             ->first();
 
+        $statistic = DB::table('t_sales')
+                        ->selectRaw("TO_CHAR(tgl, 'MM') AS month, SUM(total_bayar) AS price_month")
+                        ->groupByRaw("TO_CHAR(tgl, 'MM')")
+                        ->orderByRaw("MIN(tgl)")
+                        ->get();
+
+        $totalBalance = DB::table('t_sales')->sum('total_bayar');
+        $previousPrice = null;
+
+        foreach ($statistic as $index => $row) {
+            if($previousPrice !== null) {
+                $currentPrice = $row->price_month;
+                $growthRate =  (($currentPrice - $previousPrice) / $previousPrice) * 100;
+                $statistic[$index]->growth_rate = round($growthRate, 2);
+            } else {
+                $statistic[$index]->growth_rate = null; 
+            }
+
+            $previousPrice = $row->price_month;
+        }
+                
         $info = [
             'cards' => [
                 'jumlah_barang' => $data->jumlah_barang ?? null,
                 'jumlah_customer' => $data->jumlah_customer ?? null,
                 'jumlah_transaksi' => $data->jumlah_transaksi ?? null,
             ],
-            'statistic' => 0
+            'statistic' => $statistic->isEmpty() ? null : $statistic,
+            'total_balance' => $totalBalance ?? 0,
         ];
 
         return $this->successResponse($info, "Dashboard Views Analytic");
